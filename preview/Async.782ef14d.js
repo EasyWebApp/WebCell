57d1bdca81f522f87bfb744efba72e437a79a738
TechQuery <TechQuery@users.noreply.github.com>
2024-02-10 15:51:51 +0000

    deploy: cb56015e8b0e39896877ec506deccc91aa249d99

---
+++ preview/Async.782ef14d.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire5b94;(0,e.register)("80YIH",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var l=e("gRshD"),o=({children:e})=>(0,l.jsxs)("div",{children:["Async load: ",e]})});
+//# sourceMappingURL=Async.782ef14d.js.map
