efdc4bee6470a63d5c9318ae839c5b55269b48ce
Leo Wang <leo@wangrunlin.com>
2024-08-13 13:30:05 +0800

    chore: export all markdown file to docs

--- pack-docs.sh
+++ pack-docs.sh
@@ -1,5 +1,5 @@
 # generate multilingual file
-for file in ReadMe-*.md; do
+for file in *.md; do
     typedoc --readme $file
 
     mv docs/index.html ${file%.md}.html
@@ -8,11 +8,16 @@ done
 # generate docs
 typedoc source/
 
-# copy html file to docs folder, replace link
-for file in ReadMe-*.html; do
+mv *.html docs/
+
+# replace ReadMe-*.md to *.html, change url in *.html
+for file in docs/ReadMe-*.html; do
     # example: mv ReadMe-zh.html docs/zh.html
-    mv $file docs/"${file#ReadMe-}"
+    mv docs/$file docs/"${file#ReadMe-}"
 
     # example: replace ReadMe-zh.md zh.html
-    replace "./${file%.html}.md" "./${file#ReadMe-}" docs/*.html
+    replace "./${file%.html}" "./${file#ReadMe-}" docs/*.html
 done
+
+# default language
+mv docs/ReadMe.html docs/index.html
