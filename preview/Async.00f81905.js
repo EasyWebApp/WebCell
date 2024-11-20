4a2e410a39ac519be4dd7b27b399a8b22224048f
TechQuery <TechQuery@users.noreply.github.com>
2024-11-20 14:54:30 +0000

    deploy: 8d1df6ef74c8d0839fc441044669ec6706cdd88b

---
+++ preview/Async.00f81905.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("80YIH",function(r,l){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var i=e("zlbOT"),o=({children:e})=>(0,i.jsxs)("div",{children:["Async load: ",e]})});
+//# sourceMappingURL=Async.00f81905.js.map
