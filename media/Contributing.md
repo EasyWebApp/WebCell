664bd565fd789def33b0f937654635918242fc49
TechQuery <TechQuery@users.noreply.github.com>
2025-07-22 22:25:45 +0000

    deploy: 5d51d1c3ab0d25b1bc3b338ef5177e5b28f08519

---
+++ media/Contributing.md
@@ -0,0 +1,13 @@
+# Contributor guide
+
+## Local development
+
+```shell
+git clone https://github.com/EasyWebApp/WebCell.git ~/Desktop/WebCell
+cd ~/Desktop/WebCell
+
+npm i pnpm -g
+pnpm i
+npm test
+pnpm build
+```
---
+++ preview/Async.f4514bab.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire5b94;(0,e.register)("80YIH",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var l=e("aiwH9"),a=({children:e})=>(0,l.jsxs)("p",{children:["Async load: ",e]})});
+//# sourceMappingURL=Async.f4514bab.js.map
